e461f9d3be0b8aa7285426524ff24f773b35333c
杜卓 <dz@ioe-cn.hz>
2019-10-11 16:22:28 +0800

    创建项目

---
+++ CoreFirstStudy/CoreFirstStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreFirstStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
